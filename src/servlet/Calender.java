3b23999b5c43da870ac835c0714d62522e1ea906
SeitaRyuumon <ryuumon.seita@gmail.com>
2019-06-25 15:49:51 +0900

    fix: Calenderサーブレットクラスとindex.jspのパスを変更したから、これでcalender.htmlへ遷移できるはず

--- src/servlet/Calender.java
+++ src/servlet/Calender.java
@@ -21,7 +21,7 @@ public class Calender extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		String url = "/Calender";
+		String url = "/WEB-INF/jsp/calender.html";
 		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
 		dispatcher.forward(request, response);
 	}
