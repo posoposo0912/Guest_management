dd193095a60d54be393216148fca1cffa725165e
inotest <46962507+accounttest47@users.noreply.github.com>
2019-07-03 14:34:16 +0900

    Add files via upload

---
+++ form/thanks.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<title>イートンちどり保育園</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div><h1>イートンちどり保育園</h1></div>
+<div><h2>お問い合わせ完了</h2> </div>
+<div>
+        <div>
+		<h1>お問い合わせ 送信完了</h1>
+		<p>
+		お問い合わせありがとうございました。<br>
+		内容を確認のうえ、回答させて頂きます。<br>
+		ありがとうございました。
+		</p>
+		<!--0a href="input.php"></a>-->
+			<button type="button">ページを閉じる</button>
+
+	</div>
+</div>
+</body>
+</html>
