5f8453e8b8555adf9ac1b0bc833e4a1e5f4c4661
oguogupu <50602078+oguogupu@users.noreply.github.com>
2019-07-19 09:55:08 +0900

    Add files via upload

--- src/model/MailForm.java
+++ src/model/MailForm.java
@@ -1,20 +1,31 @@
 package model;
 
-public class MailForm {
+public class MailForm  {
 	// FIXME email �`���ŃN���X���g��
 	public String email;
+	public String name;
+	
+	public MailForm() {}
 
-	/**
-	 * @return the email
-	 */
-	public String getEmail() {
-		return email;
+	public MailForm(String name, String email) {
+		this.name = name;
+		this.email = email;
 	}
-
+	
 	/**
 	 * @param email the email to set
 	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
+	public void setName(String name) {
+		this.name = name;
+		
+	}
+	/**
+	 * @return the email
+	 */
+	public String getEmail() { return email; }
+	public String getName() { return name; }
 }
