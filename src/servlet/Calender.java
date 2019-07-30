7239d08c16954dcb03239e5ea70b7580b1e8c213
matsuda <38546447+posoposo55@users.noreply.github.com>
2019-07-30 18:47:14 +0900

    MySQL（ローカル）から日付等持ってきて表示（現状classNotFoundでDB接続不可）

--- src/servlet/Calender.java
+++ src/servlet/Calender.java
@@ -4,15 +4,18 @@ import java.io.IOException;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.CalenderDAO;
+import model.CalenderData;
 
 /**
  * Servlet implementation class Calender
  */
-@WebServlet("/Calender")
+
 public class Calender extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
@@ -22,6 +25,14 @@ public class Calender extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		String url = "/WEB-INF/jsp/calender.jsp";
+		HttpSession session = request.getSession();
+
+		CalenderDAO dao = new CalenderDAO();
+		CalenderData result = dao.connectDB();
+		if(result !=null) {
+			session.setAttribute("calender", result);
+
+		}
 		RequestDispatcher dispatcher = request.getRequestDispatcher(url);
 		dispatcher.forward(request, response);
 	}
